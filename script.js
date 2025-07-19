78ca88c9d01b7f870a9d5bbeb1cc21433c7f19bd
Vinaya-16 <vinayapatole16@gmail.com>
2025-07-19 22:11:34 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
+const { createElement } = require("react");
+
 const todoinput = document.getElementById("todo-input");
 const addbtn = document.getElementById("btn");
 const list = document.getElementById("todo-list");
+const history = document.getElementById("history");
 
-addbtn.addEventListener("click",addtask);
-todoinput.addEventListener("click",(e)=>{
-    if(e.key === "Enter"){
+addbtn.addEventListener("click", addtask);
+todoinput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
         addtask();
     }
 })
 
-function addtask(){
+function addtask() {
     const todotext = todoinput.value.trim();
-    if(todotext === "")
-    {
+    if (todotext === "") {
         alert("Please Enter something!");
         return;
     }
-    else
-    {
-        const listitem = document.createElement("li");
-        listitem.innerHTML = `
-        <span>${todotext}</span>
-        `;
-        list.appendChild(listitem);
+    else {
+        storage(todotext);
+        // const listitem = document.createElement("li");
+        // listitem.innerHTML = `
+        // <span>${todotext}</span>
+        // `;
+        // list.appendChild(listitem);
         todoinput.value = "";
     }
 
 }
 
+function storage(taskitem){
+    localStorage.setItem(Date.now(),JSON.stringify(taskitem));
+}
+
+history.addEventListener("click",viewhis);
+
+
+
+// localStorage.clear();
 
 
 
