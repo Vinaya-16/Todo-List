28cd76624b08963329fa99984d8eb066eba57321
Vinaya-16 <vinayapatole16@gmail.com>
2025-07-20 13:30:19 +0530

    Initial commit

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ function addtask() {
     }
     else {
         storage(todotext);
-        // const listitem = document.createElement("li");
-        // listitem.innerHTML = `
-        // <span>${todotext}</span>
-        // `;
-        // list.appendChild(listitem);
+        const listitem = document.createElement("li");
+        listitem.innerHTML = `
+        <span>${todotext}</span>
+        `;
+        list.appendChild(listitem);
         todoinput.value = "";
     }
 
@@ -34,8 +34,7 @@ function storage(taskitem){
     localStorage.setItem(Date.now(),JSON.stringify(taskitem));
 }
 
-history.addEventListener("click",viewhis);
-
+// history.addEventListener("click",viewhis);
 
 
 // localStorage.clear();
